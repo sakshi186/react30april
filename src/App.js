9d5c510cdcfeafc8b66ef9531bdf6f5582fbf2dc
sakshi186 <sakshisisodiya@gmail.com>
2022-05-05 01:00:54 +0530

    authenticated data store

--- src/App.js
+++ src/App.js
@@ -22,11 +22,12 @@ let myFunction =()=>{
     }
   }
    //promise chain
-   fetch('http://localhost:1337/api/students',{
+   fetch('http://localhost:1337/api/friends',{
      method:'POST',
      headers: {
+      'Authorization':'Bearer a4d8199c6b5d4f3d21a1ea25227c78ae4f4de1924909c2169379f3388a1d3ecbca4fc014a478adbe7bea8610ed2f3953bca8667be768e0582081b58130c2f46106221a0dc8f64aec321b4098619c03871ba39d586601c0d94383111782135a43b786fbe0179e3e30666376961a8882250f1bd7cf3d1e77dfb97dc13dd3330ccf',
       'Content-Type': 'application/json',
-      'accept': 'application/json'
+      'Accept': 'application/json'
      },
      body:JSON.stringify(data)
    }).then((d)=>{
